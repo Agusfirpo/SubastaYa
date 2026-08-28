9926c50ed277fcce3c1fc56b0e2c49ac30919a92
agus <agus@DESKTOP-3987475>
2026-08-28 00:52:55 -0300

    Entidades Billetera, puja y categoria. Enums EstadoSubasta y TipoTransaccion.

---
+++ Dominio/Enums/TipoTransaccion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Enums
+{
+    public enum TipoTransaccion
+    {
+        Deposito = 1,
+        Retencion = 2,
+        Liberacao = 3,
+        Pago = 4,
+        Cobro= 5
+    }
+}
