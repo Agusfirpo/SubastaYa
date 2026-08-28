c393c6d758c9204e1f794d619188c09aad4122d4
agus <agus@DESKTOP-3987475>
2026-08-28 01:16:00 -0300

    Se agrega Usings y se corrige un doble atributo

--- Dominio/Entities/TransaccionLedger.cs
+++ Dominio/Entities/TransaccionLedger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio.Enums;
 
 namespace Dominio.Entities
 {
@@ -17,8 +18,6 @@ namespace Dominio.Entities
         public TipoTransaccion Tipo {  get; set; }
 
         public decimal Monto { get; set; }  
-
-        public decimal Monto {  set; get; }
 
         public DateTime Fecha { get; set; } 
 
