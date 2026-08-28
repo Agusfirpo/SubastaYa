b69f22fc2cd516f755ee2764c0df408314b90b51
agus <agus@DESKTOP-3987475>
2026-08-27 23:50:48 -0300

    Arquitectura Hexagonal, entidades y entidad de usuario hecha.

---
+++ Dominio/Entities/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+
+        public Billetera? Billetera { get; set; }
+
+        public IList<Subasta> subasta { get; set; }= new List<Subasta>();
+        public IList<Puja> Puja { get; set; }= new List<Puja>();
+
+        public IList <AuditoriaLog> AuditoriaLogs { get; set; }= new List<AuditoriaLog>();
+
+
+    }
+}
