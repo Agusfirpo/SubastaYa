6b8eeb27ff3158c7ac01d493fc1d39f5dc195ab2
Malena Cadavid <malenacadavid11@gmail.com>
2026-08-29 23:26:02 -0300

    carpeta request y response

---
+++ Aplicacion/DTOs/Response/SubastaResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.DTOs.Response
+{
+    internal class SubastaResponse
+    {
+    }
+}
