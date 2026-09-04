aa92a73a028741cb491979b4ded0cc7db2e6446c
Malena Cadavid <malenacadavid11@gmail.com>
2026-09-04 13:36:01 -0300

    login front y modificaciones

--- Front_SubastaYa/Program.cs
+++ Front_SubastaYa/Program.cs
@@ -3,13 +3,14 @@ using Front_SubastaYa.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Front_SubastaYa.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
-
+builder.Services.AddScoped<SesionUsuarioService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7287/") });
 
 await builder.Build().RunAsync();
---
+++ Front_SubastaYa/service/SesionUsuarioService.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using Aplicacion.DTOs.Response;
+using Microsoft.JSInterop;
+
+namespace Front_SubastaYa.Services;
+
+public class SesionUsuarioService
+{
+    private readonly IJSRuntime _js;
+
+    public LoginResponse? Usuario { get; private set; }
+
+    public bool EstaLogueado => Usuario != null;
+
+    public SesionUsuarioService(IJSRuntime js)
+        => _js = js;
+
+    public async Task IniciarSesion(LoginResponse usuario)
+    {
+        Usuario = usuario;
+
+        await _js.InvokeVoidAsync(
+            "sessionStorage.setItem",
+            "usuario",
+            JsonSerializer.Serialize(usuario));
+    }
+
+    public async Task CargarSesion()
+    {
+        var json = await _js.InvokeAsync<string?>(
+            "sessionStorage.getItem",
+            "usuario");
+
+        Usuario = string.IsNullOrWhiteSpace(json)
+            ? null
+            : JsonSerializer.Deserialize<LoginResponse>(json);
+    }
+
+    public async Task CerrarSesion()
+    {
+        Usuario = null;
+
+        await _js.InvokeVoidAsync(
+            "sessionStorage.removeItem",
+            "usuario");
+    }
+}
