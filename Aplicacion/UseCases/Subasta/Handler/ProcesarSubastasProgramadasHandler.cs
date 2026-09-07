d4561f81c87a52e4ffb93100e64c9f11fdc262c4
agus <agus@DESKTOP-3987475>
2026-09-07 01:07:57 -0300

    se modifica procesarsubastahandler

--- Aplicacion/UseCases/Subasta/Handler/ProcesarSubastasProgramadasHandler.cs
+++ Aplicacion/UseCases/Subasta/Handler/ProcesarSubastasProgramadasHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Aplicacion.Interfaces.Repositories;
 using Aplicacion.UseCases.Subasta.Command;
+using Dominio.Entities;
 using Dominio.Enums;
 
 namespace Aplicacion.UseCases.Subasta.Handler
@@ -8,11 +9,12 @@ namespace Aplicacion.UseCases.Subasta.Handler
     {
         private readonly ISubastaRepository _subastaRepository;
         private readonly IUnidadTrabajo _unidadTrabajo;
-
-        public ProcesarSubastasProgramadasHandler(ISubastaRepository subastaRepository,IUnidadTrabajo unidadTrabajo)
+        private readonly IAuditoriaRepository _auditoriaRepository;
+        public ProcesarSubastasProgramadasHandler(ISubastaRepository subastaRepository,IUnidadTrabajo unidadTrabajo, IAuditoriaRepository auditoriaRepository)
         {
             _subastaRepository = subastaRepository;
             _unidadTrabajo = unidadTrabajo;
+            _auditoriaRepository = auditoriaRepository;
         }
 
         public async Task Handle(ProcesarSubastasProgramadasCommand command)
@@ -25,6 +27,18 @@ namespace Aplicacion.UseCases.Subasta.Handler
                 {
                     subasta.Estado = EstadoSubasta.Activa;
                     subasta.Version++;
+
+                    await _auditoriaRepository.AgregarAsync(
+                        new AuditoriaLog
+                        {
+                            Entidad = "Subasta",
+                            EntidadId = subasta.Id,
+                            Accion = "INICIO_SUBASTA",
+                            UsuarioId = null,
+                            DetalleJson =
+                                "{\"estadoAnterior\":\"Programada\",\"estadoNuevo\":\"Activa\"}",
+                            Fecha = command.FechaActual
+                        });
                 }
             });
         }
