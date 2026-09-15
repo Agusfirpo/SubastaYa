051805f6c3a16975b71ea52f9e2d69121203361f
agus <agus@DESKTOP-3987475>
2026-09-15 02:35:12 -0300

    se agregar carpetas

--- Application/UseCases/Bid/Handler/GetParticipationsHandler.cs
+++ Application/UseCases/Bid/Handler/GetParticipationsHandler.cs
@@ -1,6 +1,5 @@
 using Application.DTOs.Response;
 using Application.Interfaces.Repositories;
-using Application.Mappers;
 using Application.UseCases.Puja.Queries;
 using Domain.Enums;
 using System;
@@ -8,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Mappers;
 
 namespace Application.UseCases.Puja.Handler
 {
@@ -19,17 +19,23 @@ namespace Application.UseCases.Puja.Handler
         {
             _pujaRepository = pujaRepository;
         }
-
-        public async Task<IList<ParticipationResponse>> Handle(GetParticipationsQuery query,CancellationToken cancellationToken)
+        public async Task<IList<ParticipationResponse>> Handle(GetParticipationsQuery query)
         {
-            var pujas =await _pujaRepository.ObtenerPorCompradorIdAsync(query.CompradorId,cancellationToken);
+            var pujas =await _pujaRepository.ObtenerPorCompradorIdAsync(query.CompradorId);
+            var participaciones = pujas.GroupBy(p => p.SubastaId);
+            var resultado = new List<ParticipationResponse>();
 
-            return pujas.GroupBy(p => p.SubastaId)
+            return pujas
+                .GroupBy(p => p.SubastaId)
                 .Select(grupo =>
                 {
-                    var ultimaPujaUsuario = grupo.OrderByDescending(p => p.FechaPuja).First();
+                    var ultimaPujaUsuario = grupo
+                        .OrderByDescending(p => p.FechaPuja)
+                        .First();
 
-                    return BidMapper.ToParticipacionResponse(ultimaPujaUsuario);
+                    return BidMapper
+                        .ToParticipacionResponse(
+                            ultimaPujaUsuario);
                 })
                 .ToList();
         }
